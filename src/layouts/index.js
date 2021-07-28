2adc79e0e48ba61d02498c1860a3ef9ff130817b
charwaii <61100546+charwaii@users.noreply.github.com>
2021-07-28 22:31:53 +1000

    update

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // import Header from "../components/header/header";
-import Header from "../components/header2/header";
+import Header2 from "../components/header2/header2";
 import Transition from "../components/Transition/Transition";
 
 import "./layout.css";
@@ -10,7 +10,7 @@ const Layout = (props) => {
   return (
     <Transition location={props.location}>
       <div className="layout">
-        <Header />
+        <Header2 />
         {props.children}
       </div>
     </Transition>
