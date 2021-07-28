47631213c2d35b92e3b9300084fd2ae69a580d52
charwaii <61100546+charwaii@users.noreply.github.com>
2021-07-28 22:23:03 +1000

    update

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-import Header from "../components/header/header";
+// import Header from "../components/header/header";
+import Header from "../components/header2/header";
 import Transition from "../components/Transition/Transition";
 
 import "./layout.css";
